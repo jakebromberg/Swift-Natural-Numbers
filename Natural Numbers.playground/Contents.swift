17bc3aec01706f1d28fbfea45a566656a20d166e
Jake Bromberg <jake.bromberg@gmail.com>
2015-05-27 16:30:23 -0700

    adding max for nat

--- Natural Numbers.playground/Contents.swift
+++ Natural Numbers.playground/Contents.swift
@@ -27,14 +27,15 @@ func ==(a : Natural, b : Natural) -> Bool {
 	}
 }
 
-extension Natural : Equatable {
-}
-
 let One : Natural = .Successor(Box(.Zero))
 let Two : Natural = .Successor(Box(One))
 
 assert(Natural.Zero == Natural.Zero, "Natural.Zero == Natural.Zero")
 assert(One == One, "One == One")
+
+extension Natural : Equatable {
+}
+
 assert(One != Two, "One != Two")
 
 func +(a : Natural, b : Natural) -> Natural {
@@ -47,6 +48,7 @@ func +(a : Natural, b : Natural) -> Natural {
 }
 
 assert(Natural.Zero + One == One, "Natural.Zero + One == One")
+assert(One + One == Two, "One + One == Two")
 
 func -(a : Natural, b : Natural) -> Natural {
 	switch (a, b) {
@@ -55,6 +57,9 @@ func -(a : Natural, b : Natural) -> Natural {
 	default: println("EXIT_FAILURE"); exit(EXIT_FAILURE);
 	}
 }
+
+assert(.Zero - .Zero == .Zero, "(.Zero - .Zero) == .Zero")
+assert((Two - One) == One, "(Two - One) == One")
 
 func min(a : Natural, b : Natural) -> Natural {
 	switch (a, b) {
@@ -67,26 +72,30 @@ func min(a : Natural, b : Natural) -> Natural {
 
 assert(min(One, Two) == One, "min(One, Two) == One")
 
+func max(a : Natural, b : Natural) -> Natural {
+	return min(a, b) == a ? b : a
+}
+
+assert(max(One, Two) == Two, "max(One, Two) == Two")
+
 func <(a : Natural, b : Natural) -> Bool {
 	return min(a, b) == a
 }
+
+assert(One < Two, "One < Two")
 
 func <=(a : Natural, b : Natural) -> Bool {
 	return a == b || a < b
 }
 
+assert(Natural.Zero <= .Zero, "Natural.Zero <= .Zero")
+assert(One <= Two, "One < Two")
+
 func >(a : Natural, b : Natural) -> Bool {
 	return b < a
 }
 
-assert(One != Two, "One != Two")
-assert(One + One == Two, "One + One == Two")
-assert(One < Two, "One < Two")
 assert(Two > One, "Two > One")
-
-assert(Natural.Zero <= .Zero, "Natural.Zero <= .Zero")
-assert((Two - One) == One, "(Two - One) == One")
-assert((.Zero - .Zero) == .Zero, "(.Zero - .Zero) == .Zero")
 
 struct Integer {
 	let a : Natural
@@ -100,35 +109,46 @@ extension Integer {
 	}
 }
 
+func ==(a : Integer, b : Integer) -> Bool {
+	return a.a + b.b == a.b + b.a
+}
+
+assert(Integer(One) == Integer(One), "Integer(One) == Integer(One)")
+
+extension Integer : Equatable {
+}
+
+assert(Integer(Two) != Integer(One), "Integer(Two) != Integer(One)")
+
 func +(a : Integer, b : Integer) -> Integer {
 	return Integer(a: a.a + b.a, b: a.b + b.b)
 }
 
-prefix func -(a : Integer) -> Integer {
-	return Integer(a: a.b, b: a.a)
-}
-
-func -(a : Integer, b : Integer) -> Integer {
-	return a + -b
-}
+assert(Integer(Two) == Integer(One) + Integer(One), "Integer(Two) == Integer(One) + Integer(One)")
 
 prefix func -(a : Natural) -> Integer {
 	return Integer(a: .Zero, b: a)
 }
 
+assert(-One + -One == -Two, "-One + -One == -Two")
+
+prefix func -(a : Integer) -> Integer {
+	return Integer(a: a.b, b: a.a)
+}
+
+assert(-Integer(One) + -Integer(One) == -Integer(Two), "-One + -One == -Two")
+
+func -(a : Integer, b : Integer) -> Integer {
+	return a + -b
+}
+
+assert(-One == -Integer(One), "-One == -Integer(One)")
+assert(-(-One) == Integer(One), "-(-One) == Integer(One)")
+assert(-Natural.Zero == Integer(Natural.Zero), "-Natural.Zero == Integer(Natural.Zero)")
+assert(-One == Integer(One) - Integer(Two), "-One == Integer(One) - Integer(Two)")
+
 func -(a : Natural, b : Natural) -> Integer {
 	return Integer(a: a, b: b)
 }
 
-func ==(a : Integer, b : Integer) -> Bool {
-	return a.a + b.b == a.b + b.a
-}
-
-assert(Integer(Two) == Integer(One) + Integer(One), "Integer(Two) == Integer(One) + Integer(One)")
-assert(-(-One) == Integer(One), "-(-One) == Integer(One)")
-assert(-One == Integer(One) - Integer(Two), "-One == Integer(One) - Integer(Two)")
-assert(-One == (One - Two), "-One == (One - Two)")
-assert(-Natural.Zero == Integer(Natural.Zero), "-Natural.Zero == Integer(Natural.Zero)")
-
-
-assert(-One + -One == -Two, "-One + -One == -Two")
+assert(-One == One - Two, "-One == One - Two")
